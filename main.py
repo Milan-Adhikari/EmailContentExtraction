e1c07e53574f72c684b8d070956851ae047ea7dc
Milan <adwin00001@gmail.com>
2023-01-15 00:42:45 +0545

    added some comments and readme file

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from datetime import datetime
 import unicodedata
 import imaplib
 import string
 import email
 import os
-
-from datetime import datetime
 
 DATE_F = '%d-%b-%Y'
 
@@ -20,7 +19,6 @@ def get_new_attachments(email_address: str, password:str , white_list: list[str]
         end_time (str): final time to look email till
         delete_read (bool): if to delete the mail or not
     """
-    
     #Check if the attachments folder exists or not, if not then create the folder to store the downloaded attachments
     if not os.path.isdir(f"{os.getcwd()}/{file_path}"):
         try:
@@ -30,21 +28,22 @@ def get_new_attachments(email_address: str, password:str , white_list: list[str]
             print(f"Couldnt make the attachments folder in the current directory. Error: {e}")
             return
     
+    # connect to the gmail account
     mail = imaplib.IMAP4_SSL("imap.gmail.com")
     mail.login(email_address, password)
-    mail.select("inbox")
+    mail.select("inbox")  # selecting the inbox folder
     
-    _, data = mail.uid('search', None, '(SINCE "{}" BEFORE "{}")'.format(start_time, end_time))
+    _, data = mail.uid('search', None, '(SINCE "{}" BEFORE "{}")'.format(start_time, end_time))  # searching for the emails arrived between the start and end time
     inbox_item_list = data[0].split()
     
-    attachments_saved = 0
+    attachments_saved = 0  # number of attachments saved
     for item in inbox_item_list:
         _, email_data = mail.uid('fetch', item, '(RFC822)')
         raw_email = email_data[0][1].decode("utf-8")
         email_message = email.message_from_string(raw_email)
         print(email.utils.parseaddr(email_message['From'])[1])
-        if email.utils.parseaddr(email_message['From'])[1] in white_list:
-            for part in email_message.walk():
+        if email.utils.parseaddr(email_message['From'])[1] in white_list:  # executed only if the email is in white list
+            for part in email_message.walk():   
                 if part.get_content_maintype() == 'multipart':
                     continue
                 if part.get('Content-Disposition') is None:
@@ -60,7 +59,7 @@ def get_new_attachments(email_address: str, password:str , white_list: list[str]
                     save_path = os.path.join(f"{os.getcwd()}/{file_path}/{safe_file_string}", f"{attachments_saved}.{file_name.split('.')[-1]}")
                     with open(save_path, 'wb') as f:
                         f.write(part.get_payload(decode=True))
-                        
+    # to delete the messages after downloading the attachments        
     if delete_read:
         for item in inbox_item_list:
             mail.uid('store', item, '+FLAGS', '\\Deleted')
@@ -79,22 +78,22 @@ def clean_filename(filename, whitelist=valid_filename_chars, replace=' '):
     # keep only whitelisted chars
     cleaned_filename = ''.join(c for c in cleaned_filename if c in whitelist)
     if len(cleaned_filename)>char_limit:
-        print("Warning, filename truncated because it was over {}. Filenames may no longer be unique".format(char_limit))
+        print("Warning, filename truncated because it was over {}. Filenames may no longer than".format(char_limit))
     return cleaned_filename[:char_limit]   
 
-email_id = 'test@email.com'
-pwd = '123456789'
+email_id = '<email-address>'
+pwd = '<app-password>'
 
-email_white_list = ['whitelisted.email1.com', 'whilelisted.email2.com']
+email_white_list = ['<whitelisted.email1.com>', 'whilelisted.email2.com']
 
 file_path = 'attachments'
 
-start_time = '2023-01-13 12:08:33'
-end_time = '2023-01-14 13:00:00'
+start_time = '2023-01-15 00:00:33'
+end_time = '2023-01-16 18:00:00'
 
 start_time= datetime.fromisoformat(start_time)
 end_time = datetime.fromisoformat(end_time)
 
 
-get_new_attachments(email_id, pwd, email_white_list, file_path, start_time.strftime(DATE_F), end_time.strftime(DATE_F), True)
+get_new_attachments(email_id, pwd, email_white_list, file_path, start_time.strftime(DATE_F), end_time.strftime(DATE_F), False)
 
---
+++ readme.txt
@@ -0,0 +1,9 @@
+To use the program:
+Steps:
+1. Go to the Google Account that you want to read the emails from.
+2. Turn on the 2-factor authentication
+3. Generate an App password and use that App password in the script.
+# Note: App password generated are deleted after one use in google account, so please
+save the app password in some place because you will need to feed it to the script 
+everytime you run the script. You dont need to feed the password of the google account,
+but the app password generated.
